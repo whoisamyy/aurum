7e52e3201c66b8afc2f885f8e1c470880ecb383f
whoisamyy <egegeg002@gmail.com>
2025-12-23 00:20:48 +0300

    ~ Model refactors, small and big fixes + New functionality

---
+++ core/src/test/java/lang/aurum/model/TypeTest.java
@@ -0,0 +1,33 @@
+package lang.aurum.model;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Iterator;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TypeTest {
+
+
+    @Test
+    public void getInheritanceDistance_Integer_Object_test() {
+        assertEquals(2, Type.ofClass(Integer.class).getInheritanceDistance(Type.ofClass(Object.class)));
+    }
+
+    @Test
+    public void getInheritanceDistance_String_CharSequence_test() {
+        assertEquals(1, Type.ofClass(String.class).getInheritanceDistance(Type.ofClass(CharSequence.class)));
+    }
+
+    @Test
+    public void withTypeArguments_Iterator_String_test() {
+        var stringType = Type.ofClass(String.class);
+        var iteratorType = Type.ofClass(Iterator.class);
+
+        var argedType = iteratorType.withTypeArguments(new Type[]{stringType});
+        var optionalMethod = argedType.findMethod("next");
+        assertTrue(optionalMethod.isPresent());
+        assertEquals(Type.ofClass(String.class), optionalMethod.get().returnType());
+    }
+}
