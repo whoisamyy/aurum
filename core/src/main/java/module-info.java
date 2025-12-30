f32bc028cd7dea8f03eaa11f0c4c8e3a65257e87
whoisamyy <egegeg002@gmail.com>
2025-12-30 03:20:39 +0300

    + CLI tool (to be cleaned up)

--- core/src/main/java/module-info.java
+++ core/src/main/java/module-info.java
@@ -1,6 +1,10 @@
 module aurum.core {
+    exports lang.aurum;
+    exports lang.aurum.util;
     exports lang.aurum.model;
-    exports lang.aurum.model.impl; // to aurum.parsing;
+    exports lang.aurum.model.impl;
 
     requires org.jetbrains.annotations;
+    requires kotlin.stdlib;
+    requires kotlin.reflect;
 }
