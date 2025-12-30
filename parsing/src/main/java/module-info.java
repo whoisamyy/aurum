855f430e66d78a8519718f8ac1df36e4dc3d0031
whoisamyy <egegeg002@gmail.com>
2025-12-30 03:32:21 +0300

    ~ cli tool fixes

--- parsing/src/main/java/module-info.java
+++ parsing/src/main/java/module-info.java
@@ -1,6 +1,12 @@
 module aurum.parsing {
+    exports lang.aurum.parsing to aurum.cli;
+    exports lang.aurum.parsing.stages.optimisation to aurum.cli;
+
+    opens lang.aurum.parsing;
+    opens lang.aurum.parsing.stages.optimisation;
+
     requires aurum.ir;
-//    requires aurum.core;
+    requires aurum.core;
 
     requires org.antlr.antlr4.runtime;
     requires org.jetbrains.annotations;
