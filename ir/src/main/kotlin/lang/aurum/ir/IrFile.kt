8aa70fde856a981fedf79f5818e5b5576da591eb
whoisamyy <egegeg002@gmail.com>
2026-01-29 00:09:28 +0300

    + New fields that make sense

--- ir/src/main/kotlin/lang/aurum/ir/IrFile.kt
+++ ir/src/main/kotlin/lang/aurum/ir/IrFile.kt
@@ -2,8 +2,11 @@ package lang.aurum.ir
 
 import lang.aurum.model.Member
 import lang.aurum.model.Type
+import java.nio.file.Path
 
 data class IrFile(
+    val srcPath: Path,
+    val constantPool: ConstantPool,
     val classes: List<Type>,
     val fileMembers: List<Member>
 )
