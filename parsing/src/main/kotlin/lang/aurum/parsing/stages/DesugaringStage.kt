d58ad701c41f209b237f3604b708d10caa8f544a
whoisamyy <egegeg002@gmail.com>
2026-01-29 00:03:26 +0300

    + Refactors

--- parsing/src/main/kotlin/lang/aurum/parsing/stages/DesugaringStage.kt
+++ parsing/src/main/kotlin/lang/aurum/parsing/stages/DesugaringStage.kt
@@ -4,24 +4,56 @@ import lang.aurum.ir.CodeAttribute
 import lang.aurum.ir.Reference
 import lang.aurum.model.Member
 import lang.aurum.model.Method
+import lang.aurum.model.Type
 import lang.aurum.parsing.antlr.AurumParser
 import lang.aurum.parsing.attribute.PrimaryConstructorAttribute
 import lang.aurum.parsing.model.MutableMethod
+import lang.aurum.parsing.model.MutableType
 import lang.aurum.parsing.stages.coderesolution.IRCompiler
+import java.lang.reflect.AccessFlag
 
 class DesugaringStage(parsingContext: ParsingContext) : ParsingStage(parsingContext) {
     override fun execute(fileCtx: FileContext) {
-        fileCtx.classes.keys.flatMap { t -> t.members().toList() }
-            .forEach { m ->
-                process(DesugaringStageContext(fileCtx, m))
+        fileCtx.classes.keys.forEach { t ->
+            t.members().forEach { m ->
+                desugariseMember(MemberDesugaringContext(fileCtx, m))
             }
+            desugariseType(TypeDesugaringContext(fileCtx, t));
+        }
     }
 
-    private fun process(context: DesugaringStageContext) {
+    private fun desugariseMember(context: MemberDesugaringContext) {
         desugarImplicitConstructors(context)
     }
 
-    private fun desugarImplicitConstructors(ctx: DesugaringStageContext) {
+    private fun desugariseType(ctx: TypeDesugaringContext) {
+        createImplicitConstructors(ctx)
+    }
+
+    private fun createImplicitConstructors(ctx: TypeDesugaringContext) {
+        val type = ctx.type
+        if (type !is MutableType)
+            return
+
+        type.methods().filter { it.name() == "<init>" && it.owner() == type }
+            .ifEmpty {
+                val constructor = MutableMethod(
+                    type,
+                    "<init>",
+                    accessFlags = mutableListOf(AccessFlag.PUBLIC)
+                )
+                type.methods += constructor
+
+                val compiler = IRCompiler(ctx.fileContext, constructor)
+
+                compiler.generator.invokeConstructor(Reference.Super)
+                compiler.generator.return_()
+
+                constructor.attributes += CodeAttribute(compiler.instructions)
+            }
+    }
+
+    private fun desugarImplicitConstructors(ctx: MemberDesugaringContext) {
         if (ctx.member !is Method || ctx.member.attributes().none { it is PrimaryConstructorAttribute })
             return
 
@@ -31,11 +63,12 @@ class DesugaringStage(parsingContext: ParsingContext) : ParsingStage(parsingCont
         val owner = constructor.owner()
         for (field in params) {
             compiler.generator.putField(
-                Reference("this"),
+                Reference.This,
                 compiler.constantPool.getReference(owner.findField(field.name()).orElseThrow()),
                 compiler.currentScope[field.name()]!!.toReference()
             )
         }
+        compiler.generator.return_()
 
         constructor.attributes += CodeAttribute(compiler.instructions)
     }
@@ -67,7 +100,12 @@ class DesugaringStage(parsingContext: ParsingContext) : ParsingStage(parsingCont
     }
 }
 
-data class DesugaringStageContext (
+data class MemberDesugaringContext (
     val fileContext: FileContext,
     val member: Member // member can be created or changed
+) : AbstractParsingContext()
+
+data class TypeDesugaringContext (
+    val fileContext: FileContext,
+    val type: Type // member can be created or changed
 ) : AbstractParsingContext()
