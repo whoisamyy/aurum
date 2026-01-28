4939519a52e23ba4b8e2595df6601cc9e049df34
whoisamyy <egegeg002@gmail.com>
2026-01-28 23:53:36 +0300

    + (Limited) lambdas support

--- parsing/src/main/kotlin/lang/aurum/parsing/stages/coderesolution/CodeResolutionStage.kt
+++ parsing/src/main/kotlin/lang/aurum/parsing/stages/coderesolution/CodeResolutionStage.kt
@@ -2,12 +2,11 @@ package lang.aurum.parsing.stages.coderesolution
 
 import lang.aurum.ir.CodeAttribute
 import lang.aurum.model.Method
+import lang.aurum.parsing.antlr.AurumParser
 import lang.aurum.parsing.attribute.get
 import lang.aurum.parsing.model.MutableMethod
-import lang.aurum.parsing.stages.BlockAttribute
-import lang.aurum.parsing.stages.FileContext
-import lang.aurum.parsing.stages.ParsingContext
-import lang.aurum.parsing.stages.ParsingStage
+import lang.aurum.parsing.model.MutableTypePool
+import lang.aurum.parsing.stages.*
 
 class CodeResolutionStage(parsingContext: ParsingContext) : ParsingStage(parsingContext) {
     lateinit var fileContext: FileContext
@@ -23,6 +22,17 @@ class CodeResolutionStage(parsingContext: ParsingContext) : ParsingStage(parsing
             compiler.process(block)
 
             method.attributes += CodeAttribute(compiler.instructions)
+        }
+    }
+
+    override fun afterFileContext(fileContext: FileContext) {
+        MutableTypePool.pool.mapNotNull { (namePkgTArgs, type) ->
+            val (namePkg, _) = namePkgTArgs
+            if (namePkg.startsWith("aurum.lang.Fn"))
+                return@mapNotNull type
+            null
+        }.forEach {
+            fileContext.classes[it] = ClassDeclCtx(AurumParser.ClassDeclContext(null, 0))
         }
     }
 }
