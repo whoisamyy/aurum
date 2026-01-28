27b9d8db67aaa2ba83177faae8b3f38d52135c67
whoisamyy <egegeg002@gmail.com>
2026-01-29 00:03:40 +0300

    + Refactors

--- parsing/src/main/kotlin/lang/aurum/parsing/stages/BlockAttribute.kt
+++ parsing/src/main/kotlin/lang/aurum/parsing/stages/BlockAttribute.kt
@@ -6,7 +6,7 @@ import lang.aurum.parsing.antlr.AurumParser
 data class BlockAttribute(val block: AurumParser.BlockContext) : Attribute {
     override fun name(): String = "Block"
 
-    override fun values(): Map<String?, Any?> = mapOf("block" to block)
+    override fun values(): Map<String, Any?> = mapOf("block" to block)
 
     override fun isVisible(): Boolean = false
 }
