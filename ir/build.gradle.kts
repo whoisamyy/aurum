7094459f8f840f3b92cbb100fb3dba7f43321e60
whoisamyy <egegeg002@gmail.com>
2026-01-29 00:10:41 +0300

    ~ Import fixes

--- ir/build.gradle.kts
+++ ir/build.gradle.kts
@@ -12,6 +12,8 @@ repositories {
 dependencies {
     implementation(project(":core"))
 
+    implementation("org.jetbrains:annotations:26.0.2")
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
