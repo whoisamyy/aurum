7cafed1bb58714c9032a53cb17892a10b1b2c3ef
whoisamyy <egegeg002@gmail.com>
2026-02-26 16:30:04 +0300

    + export of factory package to aurum.parsing

--- core/src/main/java/module-info.java
+++ core/src/main/java/module-info.java
@@ -4,6 +4,7 @@ module aurum.core {
     exports lang.aurum.model;
     exports lang.aurum.model.util;
     exports lang.aurum.model.impl to aurum.parsing;
+    exports lang.aurum.model.factory to aurum.parsing;
 
     opens lang.aurum;
     opens lang.aurum.util;
