a95634fd3f2cd1d490febf30507380cd74058fd3
whoisamyy <egegeg002@gmail.com>
2026-04-20 00:31:58 +0300

    ~ Commented out. To be removed I think

---
+++ compiler/src/main/kotlin/aurum/lang/compiler/frontend/attribute/ExtensionAttributeImpl.kt
@@ -0,0 +1,10 @@
+package aurum.lang.compiler.frontend.attribute
+
+//class ExtensionAttributeImpl (
+//    val typeCtx: AurumParser.TypeExprContext
+//) : ExtensionAttribute() {
+//    override fun values(): Map<String, Any?> =
+//        mapOf(
+//            "typeCtx" to typeCtx
+//        )
+//}
