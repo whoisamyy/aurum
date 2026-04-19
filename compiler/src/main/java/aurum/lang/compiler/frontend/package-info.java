af8e754bfa507c96ff3795727bf901f5feecd25f
whoisamyy <egegeg002@gmail.com>
2026-04-20 00:32:18 +0300

    ~ Just package info

---
+++ compiler/src/main/java/aurum/lang/compiler/frontend/package-info.java
@@ -0,0 +1,3 @@
+/// Main function of this package is to generate IR out of given string.
+/// It handles parsing, typing, early linking with java.base and semantic analysis of the code.
+package aurum.lang.compiler.frontend;
