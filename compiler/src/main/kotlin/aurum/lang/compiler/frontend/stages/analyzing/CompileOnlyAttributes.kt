be12c2995a6744558d4485a1c852aa5481f7002a
whoisamyy <egegeg002@gmail.com>
2026-06-11 19:25:23 +0300

    ~ Some work on attributes + new marker attribute

--- compiler/src/main/kotlin/aurum/lang/compiler/frontend/stages/analyzing/CompileOnlyAttributes.kt
+++ compiler/src/main/kotlin/aurum/lang/compiler/frontend/stages/analyzing/CompileOnlyAttributes.kt
@@ -1,30 +1,32 @@
 package aurum.lang.compiler.frontend.stages.analyzing
 
 import aurum.lang.compiler.frontend.stages.parsing.ASTNode
+import aurum.lang.compiler.frontend.stages.typeresolving.AbstractTypeResolver
 import aurum.lang.model.Attribute
 
 sealed class CompileOnlyAttribute : Attribute {
     final override fun isVisible(): Boolean = false
+
+    override fun values(): Map<String, Any?> = mapOf()
 }
 
 data class DefaultValueAttribute (
     val value: ASTNode.Expression
 ) : CompileOnlyAttribute() {
     override fun name(): String = "DefaultValue"
-
-    override fun values(): Map<String, Any?> = mapOf("value" to value)
 }
 
 data class CodeBlockAttribute (
-    val codeBlock: ASTNode.CodeBlock
+    val codeBlock: ASTNode.CodeBlock,
+    val typeResolver: AbstractTypeResolver
 ) : CompileOnlyAttribute() {
     override fun name(): String = "CodeBlock"
-
-    override fun values(): Map<String, Any?> = mapOf("codeBlock" to codeBlock)
 }
 
 object OperatorTemplate : CompileOnlyAttribute() {
     override fun name(): String = "OperatorTemplate"
+}
 
-    override fun values(): Map<String, Any?> = mapOf()
+object GeneratedClassAttribute : CompileOnlyAttribute() {
+    override fun name(): String = "GeneratedClass"
 }
