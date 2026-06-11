b3121985d7bdc078b132e6b1f8a6e10e208ae6d4
whoisamyy <egegeg002@gmail.com>
2026-06-11 19:32:34 +0300

    ~ Clean-ups

--- compiler/build.gradle.kts
+++ compiler/build.gradle.kts
@@ -1,9 +1,6 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
     id("java")
     kotlin("jvm") version "2.2.0"
-    antlr
 }
 
 group = "io.github.whoisamyy"
@@ -17,7 +14,6 @@ dependencies {
     implementation(project(":core"))
     implementation(project(":ir"))
     implementation(project(":runtime"))
-    antlr("org.antlr:antlr4:4.13.2")
 
     implementation(kotlin("reflect"))
     implementation("org.jetbrains:annotations:26.0.2")
@@ -27,26 +23,7 @@ dependencies {
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
-tasks.generateGrammarSource {
-    arguments = arguments + listOf("-no-listener")
-    source = fileTree("src/main/antlr")
-}
-
-tasks.withType<KotlinCompile> {
-    dependsOn(tasks.generateGrammarSource)
-}
-
-tasks.compileTestKotlin {
-    dependsOn(tasks.generateTestGrammarSource)
-}
-
-tasks.compileTestJava {
-    dependsOn(tasks.generateTestGrammarSource)
-}
-
 tasks.withType<JavaCompile> {
-    dependsOn(tasks.generateGrammarSource)
-
     options.compilerArgumentProviders.add(object : CommandLineArgumentProvider {
         @CompileClasspath
         val kotlinClasses = kotlin.sourceSets.main.flatMap { it.kotlin.classesDirectory }
