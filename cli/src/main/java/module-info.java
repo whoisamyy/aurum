b8eaff7efd0efa1d7d0e0312489c9da6e30e653c
whoisamyy <egegeg002@gmail.com>
2026-06-11 19:34:08 +0300

    ~ clean-ups

--- cli/src/main/java/module-info.java
+++ cli/src/main/java/module-info.java
@@ -1,10 +1,9 @@
 module aurum.cli {
+    exports aurum.lang.cli.args;
+
     opens aurum.lang.cli;
 
     requires aurum.compiler;
-    requires aurum.core;
-    requires aurum.ir;
     requires kotlin.stdlib;
-    requires org.antlr.antlr4.runtime;
     requires org.jetbrains.annotations;
 }
