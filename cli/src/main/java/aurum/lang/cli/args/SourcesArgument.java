973443193ca7159e60c5486501627bcc64f37560
whoisamyy <egegeg002@gmail.com>
2026-06-11 19:33:45 +0300

    ~ Refactor

--- cli/src/main/java/aurum/lang/cli/args/SourcesArgument.java
+++ cli/src/main/java/aurum/lang/cli/args/SourcesArgument.java
@@ -1,23 +1,25 @@
 package aurum.lang.cli.args;
 
-import aurum.lang.cli.aurum.lang.cli.Command;
-import aurum.lang.cli.aurum.lang.cli.Option;
-import aurum.lang.cli.aurum.lang.cli.Parameters;
+import aurum.lang.cli.Command;
+import aurum.lang.cli.Option;
+import aurum.lang.cli.Parameters;
 import org.jetbrains.annotations.NotNull;
 
 import java.nio.file.Path;
 import java.util.Set;
 
+@SuppressWarnings("CanBeFinal")
 @Command
 public final class SourcesArgument implements Argument {
+    @Parameters
+    public Set<Path> sources = Set.of();
+
     @Option(names = {"-wd", "--workdir"})
     public Path workDir = Path.of("");
 
     @Option(names = {"-o", "--output"})
     public Path output = Path.of("output/");
 
-    @Parameters
-    public Set<Path> sources = Set.of();
 
     public @NotNull Path workDir() {
         return workDir;
