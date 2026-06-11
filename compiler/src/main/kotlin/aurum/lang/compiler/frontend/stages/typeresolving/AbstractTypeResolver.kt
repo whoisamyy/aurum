192ffb2c6908a90f4ece0be6e152ff13c14d5e78
whoisamyy <egegeg002@gmail.com>
2026-06-11 19:15:14 +0300

    + Type resolving (previously linking)

---
+++ compiler/src/main/kotlin/aurum/lang/compiler/frontend/stages/typeresolving/AbstractTypeResolver.kt
@@ -0,0 +1,59 @@
+package aurum.lang.compiler.frontend.stages.typeresolving
+
+import aurum.lang.compiler.frontend.stages.parsing.ASTNode
+import aurum.lang.model.PrimitiveType
+import aurum.lang.model.Type
+import aurum.lang.model.Types
+
+@Suppress("JavaDefaultMethodsNotOverriddenByDelegation")
+abstract class AbstractTypeResolver(availableTypes: Set<Type>) : Set<Type> by availableTypes {
+    companion object {
+        val defaultTypes = mapOf(
+            "string" to Types.STRING,
+            "object" to Types.OBJECT
+        )
+    }
+
+    constructor(parentLinker: AbstractTypeResolver, types: Set<Type>)
+            : this((types + parentLinker.availableTypes.values).toSet())
+
+    val availableTypes: Map<String, Type> = availableTypes.associateBy(Type::className) + defaultTypes
+
+    abstract fun getTypeOrNull(typeExpr: ASTNode.TypeExpr?): Type?
+    abstract fun getTypeOrNull(fullName: String?): Type?
+
+    fun getType(typeExpr: ASTNode.TypeExpr): Type {
+        return getTypeOrNull(typeExpr) ?: error("Type not found: $typeExpr")
+    }
+
+    fun getType(fullName: String): Type {
+        return getTypeOrNull(fullName) ?: error("Type not found: $fullName")
+    }
+
+    fun getPrimitiveType(name: String): PrimitiveType {
+        return getPrimitiveTypeOrNull(name) ?: error("$name is not a primitive type")
+    }
+
+    fun getPrimitiveTypeOrNull(name: String?): PrimitiveType? {
+        return when (name) {
+            "void" -> Types.VOID
+            "boolean" -> Types.BOOLEAN
+            "byte" -> Types.BYTE
+            "short" -> Types.SHORT
+            "char" -> Types.CHAR
+            "int" -> Types.INT
+            "float" -> Types.FLOAT
+            "long" -> Types.LONG
+            "double" -> Types.DOUBLE
+            else -> null
+        }
+    }
+
+    operator fun get(name: String): Type? {
+        return getTypeOrNull(name)
+    }
+
+    operator fun get(type: ASTNode.TypeExpr): Type? {
+        return getTypeOrNull(type)
+    }
+}
--- compiler/src/main/kotlin/aurum/lang/compiler/frontend/stages/typeresolving/SimpleTypeResolver.kt
+++ compiler/src/main/kotlin/aurum/lang/compiler/frontend/stages/typeresolving/SimpleTypeResolver.kt
@@ -1,30 +1,30 @@
-package aurum.lang.compiler.frontend.stages.analyzing
+package aurum.lang.compiler.frontend.stages.typeresolving
 
 import aurum.lang.compiler.frontend.model.fnType
 import aurum.lang.compiler.frontend.stages.parsing.ASTNode
 import aurum.lang.model.*
+import java.util.function.IntFunction
 
-class TypeGetter(
+open class SimpleTypeResolver(
     availableTypes: Set<Type>
-) {
-    private val availableTypes: Map<String, Type> = availableTypes.associateBy(Type::fullName)
+) : AbstractTypeResolver(availableTypes) {
     constructor(pkg: Package) : this(pkg.types().toSet())
-    constructor(typeGetter: TypeGetter, types: Set<Type>)
-            : this((types + typeGetter.availableTypes.values).toSet())
+    constructor(parentLinker: AbstractTypeResolver, types: Set<Type>)
+            : this((types + parentLinker.availableTypes.values).toSet())
 
-    fun getTypeOrNull(typeExpr: ASTNode.TypeExpr): Type? {
-        when (typeExpr) {
+    override fun getTypeOrNull(typeExpr: ASTNode.TypeExpr?): Type? {
+        return when (typeExpr) {
             is ASTNode.PlainType -> {
                 val type = getTypeOrNull(typeExpr.name.toString()) ?: return null
                 if (typeExpr.suffix == null) return type
 
-                return type.asArray(typeExpr.suffix.size)
+                type.asArray(typeExpr.suffix.size)
             }
             is ASTNode.ParenthesizedType -> {
                 val type = getTypeOrNull(typeExpr.type) ?: return null
                 if (typeExpr.suffix == null) return type
 
-                return type.asArray(typeExpr.suffix.size)
+                type.asArray(typeExpr.suffix.size)
             }
             is ASTNode.ParametrizedType -> {
                 val type = getTypeOrNull(typeExpr.name.toString()) ?: return null
@@ -41,51 +41,30 @@ class TypeGetter(
                     }
                 }
 
-                return type.withTypeArguments(typeArgs.toTypedArray())
+                type.withTypeArguments(*typeArgs.toTypedArray())
             }
             is ASTNode.LambdaType -> {
                 val paramTypes = typeExpr.paramTypes?.map(::getType) ?: emptyList()
                 val returnType = getType(typeExpr.returnType)
 
-                return fnType(returnType, paramTypes)
+                fnType(returnType, paramTypes)
             }
             is ASTNode.IntersectionType -> {
-                return IntersectionType.ofTypeModels(typeExpr.types.map(::getType).toTypedArray())
+                IntersectionType.ofTypeModels(typeExpr.types.map(::getType).toTypedArray())
             }
             is ASTNode.UnionType -> {
-                return UnionType.ofTypeModels(typeExpr.types.map(::getType).toTypedArray())
+                UnionType.ofTypeModels(typeExpr.types.map(::getType).toTypedArray())
             }
-        }
-    }
-
-    fun getType(typeExpr: ASTNode.TypeExpr): Type {
-        return getTypeOrNull(typeExpr) ?: error("Type not found: $typeExpr")
-    }
-
-    fun getType(fullName: String): Type {
-        return getTypeOrNull(fullName) ?: error("Type not found: $fullName")
-    }
-
-    fun getTypeOrNull(fullName: String): Type? {
-        return availableTypes[fullName] ?: getPrimitiveTypeOrNull(fullName)
-    }
-
-    fun getPrimitiveTypeOrNull(name: String): PrimitiveType? {
-        return when (name) {
-            "void" -> Types.VOID
-            "boolean" -> Types.BOOLEAN
-            "byte" -> Types.BYTE
-            "short" -> Types.SHORT
-            "char" -> Types.CHAR
-            "int" -> Types.INT
-            "float" -> Types.FLOAT
-            "long" -> Types.LONG
-            "double" -> Types.DOUBLE
             else -> null
         }
     }
 
-    fun getPrimitiveType(name: String): PrimitiveType {
-        return getPrimitiveTypeOrNull(name) ?: error("$name is not a primitive type")
+    override fun getTypeOrNull(fullName: String?): Type? {
+        return availableTypes[fullName] ?: getPrimitiveTypeOrNull(fullName)
+    }
+
+    @Suppress("UNCHECKED_CAST", "OVERRIDE_DEPRECATION")
+    override fun <T> toArray(generator: IntFunction<Array<out T?>?>): Array<out T?>? {
+        return availableTypes.values.toTypedArray() as Array<out T?>?
     }
 }
