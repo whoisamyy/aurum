b026719f01a0245cbcf1cbea00144f0e04870e41
whoisamyy <egegeg002@gmail.com>
2026-06-11 18:35:15 +0300

    + Some runtime and std lib

--- runtime/build.gradle.kts
+++ runtime/build.gradle.kts
@@ -13,6 +13,8 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    implementation("org.jetbrains:annotations:26.0.2")
 }
 
 tasks.test {
